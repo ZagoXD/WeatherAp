bffee437865cb43cf113b7827cdfa4bf4180becf
ZagoXD <skyziinxd@gmail.com>
2024-08-14 12:05:08 -0300

    tradução vento

--- src/components/AdditionalWeatherInfo.js
+++ src/components/AdditionalWeatherInfo.js
@@ -3,11 +3,48 @@ import { View, Text, StyleSheet } from 'react-native';
 import { theme } from '../styles/theme';
 
 const AdditionalWeatherInfo = ({ windSpeed, windDirection, aqi, uvIndex, sunrise, sunset }) => {
+  const windDirectionTranslations = {
+    "N": "Norte",
+    "NNE": "Norte-Nordeste",
+    "NE": "Nordeste",
+    "ENE": "Leste-Nordeste",
+    "E": "Leste",
+    "ESE": "Leste-Sudeste",
+    "SE": "Sudeste",
+    "SSE": "Sul-Sudeste",
+    "S": "Sul",
+    "SSW": "Sul-Sudoeste",
+    "SW": "Sudoeste",
+    "WSW": "Oeste-Sudoeste",
+    "W": "Oeste",
+    "WNW": "Oeste-Noroeste",
+    "NW": "Noroeste",
+    "NNW": "Norte-Noroeste",
+    "north": "Norte",
+    "north-northeast": "Norte-Nordeste",
+    "northeast": "Nordeste",
+    "east-northeast": "Leste-Nordeste",
+    "east": "Leste",
+    "east-southeast": "Leste-Sudeste",
+    "southeast": "Sudeste",
+    "south-southeast": "Sul-Sudeste",
+    "south": "Sul",
+    "south-southwest": "Sul-Sudoeste",
+    "southwest": "Sudoeste",
+    "west-southwest": "Oeste-Sudoeste",
+    "west": "Oeste",
+    "west-northwest": "Oeste-Noroeste",
+    "northwest": "Noroeste",
+    "north-northwest": "Norte-Noroeste",
+  };
+
+  const translatedWindDirection = windDirectionTranslations[windDirection] || windDirection;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Informações Adicionais</Text>
       <Text style={styles.infoText}>Velocidade do Vento: {windSpeed} m/s</Text>
-      <Text style={styles.infoText}>Direção do Vento: {windDirection}</Text>
+      <Text style={styles.infoText}>Direção do Vento: {translatedWindDirection}</Text>
       <Text style={styles.infoText}>Qualidade do Ar (AQI): {aqi}</Text>
       <Text style={styles.infoText}>Índice UV: {uvIndex}</Text>
       <Text style={styles.infoText}>Nascer do Sol: {sunrise}</Text>
